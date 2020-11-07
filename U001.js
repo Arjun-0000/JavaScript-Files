c969c88228b0dd1daba5b9d2da4308517ae12963
Arjun Shrestha <40835849+Arjun-0000@users.noreply.github.com>
2020-11-07 07:27:15 +0545

    Add files via upload

---
+++ U001.js
@@ -0,0 +1,22 @@
+//document.write('hello world');
+//alert('hwello world');
+/*document.write("hello");
+document.write("hello")*/
+var x = " arjun \" is my name \" Shrestha";
+/*var question1 = "what is your ";
+var question2 = prompt(question1 + " question age/name");
+var question = prompt(question1 + question2);
+*/
+
+var task = ["make a full javascript useful", "read a book", "brush a teeth", "look oustide","sleep"];
+var progress = ["done", "in progress", "to do"];
+//task[100] = "new value";
+console.log(task[(task.length - 1)])
+console.log(task);
+task[(task.length)] = "last add";
+console.log(task[(task.length-1)]);
+/*
+var q1 = prompt("which task 1 - "+ task.length +"?");
+var q2 = prompt("1-done 2-in progress 3-to do");
+console.log("today you will " + task[(q1-1)] + " and status = " + progress[(q2-1)]);
+*/
